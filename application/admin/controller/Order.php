3fe0d6908c175758e480517949168b8bc2191f40
弃身锋刃 <254278018@qq.com>
2020-04-03 23:15:32 +0800

    Update Order.php

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -44,7 +44,7 @@ class Order extends \think\Controller{
 
         //生成订单
         $order_id = OrderModel::buildOrderId($uid);
-        //插入订单
+        //插入订单 -- 这里建议使用异步队列
         $order_status =  OrderModel::addOrder($uid,$order_id,$this->good_id);
         if ($order_status){
             //mysql库存;
@@ -77,28 +77,5 @@ class Order extends \think\Controller{
         return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ad_bd568ce7058a1091"></think>';
     }
 
-    /**
-     * swoole 客户端
-     * @return string
-     */
-    public function send(){
-        $client = new \swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_SYNC);
-        $ret = $client->connect("192.168.1.120", 9500);
-
-        if(empty($ret)){
-            echo 'error!connect to swoole_server failed';
-        } else {
-            //这里只是简单的实现了发送的内容
-            $send = $client->send('我是客户端,我发送了消息~__');
-            //等待服务端返回诗句
-            if ($send){
-                //recv返回客户端消息,应该等服务端返回数据才执行,否则会报错
-                $recv = $client->recv();
-            }else{
-                $recv = "服务端返回失败!";
-            }
-
-            return $send."--".$recv;
-        }
-    }
+  
 }
